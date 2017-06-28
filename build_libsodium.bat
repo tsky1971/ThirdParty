05ff680b076196a0c26e0b6178aa345d1c7767bf
dmadmin <cglab.user@googlemail.com>
2017-06-28 15:28:09 +0200

    add copy dll

--- build_libsodium.bat
+++ build_libsodium.bat
@@ -5,3 +5,7 @@ CALL buildbase.bat ..\vs2017\libsodium.sln 15
 CALL buildbase.bat ..\vs2015\libsodium.sln 14
 ::cd ..\..\..\..
 popd
+xcopy libsodium\bin\Win32\Debug\v140\dynamic\*.* ..\bin\Win32\Debug\ /V/S/E
+xcopy libsodium\bin\Win32\Release\v140\dynamic\*.* ..\bin\Win32\Release\ /V/S/E
+xcopy libsodium\bin\x64\Debug\v140\dynamic\*.* ..\bin\x64\Debug\ /V/S/E
+xcopy libsodium\bin\x64\Release\v140\dynamic\*.* ..\bin\x64\Release\ /V/S/E
