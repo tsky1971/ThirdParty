124103322b9cb7264b307c809bf50cd73497efb9
tsky <thomas.kollakowsky@gmail.com>
2017-07-22 19:41:36 +0200

    update

--- build_OpenCV.bat
+++ build_OpenCV.bat
@@ -1,22 +1,36 @@
 echo on
+echo "build opencv"
 
-cd opencv
-cd build32
-cmake -G "Visual Studio 14 2015" ..
-cd ..
+if not exist opencv (
+	echo "you to get it first"
+	
+) else (
+	cd opencv
+	
+	if not exist build32 (
+		mkdir build32
+	)
+	cd build32
+		
+	cmake -G "Visual Studio 14 2015" ..
+	cd ..
 
-pause
+	pause
 
-cd build64
-cmake -G "Visual Studio 14 2015 Win64" ..
-cd ..
+	if not exist build64 (
+		mkdir build64
+	)
+	cd build64
+	cmake -G "Visual Studio 14 2015 Win64" ..
+	cd ..
 
-pause
+	pause
 
-cmake --build build64 --config Release
+	cmake --build build64 --config Release
 
-pause
+	pause
 
-cmake --build build32 --config Release
+	cmake --build build32 --config Release
 
-pause
+	pause
+)
