d36f1929c0968a68717fad9059d12bc4aa3769a2
tsky <thomas.kollakowsky@gmail.com>
2017-07-28 23:21:04 +0200

    update scripts vs version 2015 2017 - for simplicity laster version is copied

--- deploy_libczmq.bat
+++
@@ -1,5 +0,0 @@
-echo off
-xcopy czmq\bin\Win32\Debug\v140\dynamic\*.* ..\bin\Win32\Debug\ /V/S/E
-xcopy czmq\bin\Win32\Release\v140\dynamic\*.* ..\bin\Win32\Release\ /V/S/E
-xcopy czmq\bin\x64\Debug\v140\dynamic\*.* ..\bin\x64\Debug\ /V/S/E
-xcopy czmq\bin\x64\Release\v140\dynamic\*.* ..\bin\x64\Release\ /V/S/E
--- deploy_libsodium.bat
+++ deploy_libsodium.bat
@@ -1,7 +1,17 @@
 echo off
-xcopy libsodium\bin\Win32\Debug\v140\dynamic\*.* ..\bin\Win32\Debug\ /V/S/E/Y
-xcopy libsodium\bin\Win32\Release\v140\dynamic\*.* ..\bin\Win32\Release\ /V/S/E/Y
-xcopy libsodium\bin\x64\Debug\v140\dynamic\*.* ..\bin\x64\Debug\ /V/S/E/Y
-xcopy libsodium\bin\x64\Release\v140\dynamic\*.* ..\bin\x64\Release\ /V/S/E/Y
+
+if exist libsodium\bin\Win32\Debug\v140\ (
+	xcopy libsodium\bin\Win32\Debug\v140\dynamic\*.* ..\bin\Win32\Debug\ /V/S/E/Y
+	xcopy libsodium\bin\Win32\Release\v140\dynamic\*.* ..\bin\Win32\Release\ /V/S/E/Y
+	xcopy libsodium\bin\x64\Debug\v140\dynamic\*.* ..\bin\x64\Debug\ /V/S/E/Y
+	xcopy libsodium\bin\x64\Release\v140\dynamic\*.* ..\bin\x64\Release\ /V/S/E/Y
+)
+
+if exist libsodium\bin\Win32\Debug\v141\ (
+	xcopy libsodium\bin\Win32\Debug\v141\dynamic\*.* ..\bin\Win32\Debug\ /V/S/E/Y
+	xcopy libsodium\bin\Win32\Release\v141\dynamic\*.* ..\bin\Win32\Release\ /V/S/E/Y
+	xcopy libsodium\bin\x64\Debug\v141\dynamic\*.* ..\bin\x64\Debug\ /V/S/E/Y
+	xcopy libsodium\bin\x64\Release\v141\dynamic\*.* ..\bin\x64\Release\ /V/S/E/Y
+)
 
 xcopy libsodium\src\libsodium\include\*.h ..\include\ /V/E/S/Y
