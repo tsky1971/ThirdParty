9419f2d3b4f525d7660cf2dd6353aaea766fb792
tsky <thomas.kollakowsky@gmail.com>
2017-08-01 21:59:33 +0200

    improve scripts

--- build_libsodium.bat
+++ build_libsodium.bat
@@ -1,7 +1,8 @@
+echo off
 
 pushd libsodium\builds\msvc\build
-rem buildall.bat
-CALL buildbase.bat ..\vs2017\libsodium.sln 15
+buildall.bat
+::CALL buildbase.bat ..\vs2017\libsodium.sln 15
 ::CALL buildbase.bat ..\vs2015\libsodium.sln 14
 ::cd ..\..\..\..
 popd
