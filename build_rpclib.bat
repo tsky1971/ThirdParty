db547785d23eb95f4a4e6916bc99f79bad5effe0
tsky <thomas.kollakowsky@gmail.com>
2018-01-07 12:55:47 +0100

    add msgpack get build deploy

--- build_rpclib.bat
+++ build_rpclib.bat
@@ -5,27 +5,18 @@ pushd rpclib
 
 git pull
 
-pause
-
 pushd build32
 cmake -G "Visual Studio 15 2017"  -Wno-dev ..
 popd
 
-pause
+cmake --build build32 --config Release
 
 pushd build64
 cmake -G "Visual Studio 15 2017 Win64"  -Wno-dev ..
 popd
 
-pause
-
-cmake --build build32 --config Release
-
-pause
-
 cmake --build build64 --config Release
 
 popd
 
-pause
 
