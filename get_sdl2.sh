8368f5881ee8d118f4c267cfc12f06c9baf59a06
tsky <thomas.kollakowsky@w-hs.de>
2018-01-14 22:28:08 +0100

    get all sdl libs

--- get_sdl2.sh
+++ get_sdl2.sh
@@ -1,7 +1,68 @@
-hg clone https://hg.libsdl.org/SDL SDL2
-cd SDL2
-mkdir build
-cd build
-../configure
-make
-#sudo make install
+hg clone https://hg.libsdl.org/SDL
+cd SDL
+	mkdir build
+	cd build
+		../configure
+		make
+		sudo make install
+	cd ..
+cd ..
+
+hg clone https://hg.libsdl.org/SDL_image
+cd SDL_image
+	mkdir build
+	./autogen.sh
+	cd build
+		../configure
+		make
+		sudo make install
+	cd ..
+cd ..
+
+hg clone https://hg.libsdl.org/SDL_mixer
+cd SDL_mixer
+	mkdir build
+	cd build
+		../configure
+		make
+		sudo make install
+	cd ..
+cd ..
+
+
+hg clone https://hg.libsdl.org/SDL_net
+cd SDL_net
+        mkdir build
+	./autogen.sh
+        cd build
+                ../configure
+                make
+                sudo make install
+        cd ..
+cd ..
+
+
+hg clone https://hg.libsdl.org/SDL_ttf
+cd SDL_ttf
+        mkdir build
+	./autogen.sh
+        cd build
+                ../configure
+                make
+                sudo make install
+        cd ..
+cd ..
+
+
+hg clone https://hg.libsdl.org/SDL_rtf
+cd SDL_rtf
+#        mkdir build
+#	./autogen.sh
+ #       cd build
+  #              ../configure
+   #             make
+    #            sudo make install
+     #   cd ..
+cd ..
+
+
