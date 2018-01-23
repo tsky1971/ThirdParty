da9025123fa940b591fb0706e368a8a99b4e8328
tsky <thomas.kollakowsky@gmail.com>
2018-01-23 23:35:37 +0100

    czmq not working with vc2017

--- build_libczmq.bat
+++ build_libczmq.bat
@@ -1,5 +1,11 @@
-::cd czmq\builds\msvc
-::configure.bat --without-zmakecert --disable-drafts --without-zsp --without-czmq_selftest
-cd czmq\builds\msvc\vs2015
-call build.bat
-cd ..\..\..\..
+REM pushd czmq\builds\msvc
+REM configure.bat --without-zmakecert --disable-drafts --without-zsp --without-czmq_selftest
+REM popd
+
+REM cd czmq\builds\msvc\vs2015
+REM call build.bat
+REM cd ..\..\..\..
+
+echo build manual
+
+pause
