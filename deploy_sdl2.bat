30552105945b983f9602e215c480d8d2c1ab9bd3
tsky <thomas.kollakowsky@gmail.com>
2018-01-23 23:09:31 +0100

    update deploy copy

--- deploy_sdl2.bat
+++ deploy_sdl2.bat
@@ -2,3 +2,9 @@ echo off
 
 xcopy SDL\include ..\include\SDL\ /V/S/E/Y
 xcopy SDL\build64\Release ..\bin\x64\Release /V/S/E/Y
+
+copy SDL_image\SDL_image.h ..\include\SDL\SDL_image.h /Y
+xcopy SDL_image\VisualC\x64\Release ..\bin\x64\Release /V/S/E/Y
+
+copy SDL_net\SDL_net.h ..\include\SDL\SDL_net.h /Y
+xcopy SDL_net\VisualC\x64\Release ..\bin\x64\Release /V/S/E/Y
